0ac9d6669f3733875659e6173295c87c5985f623
cedvid <cedricvidot@gmail.com>
2023-07-31 15:04:52 +0200

    add result in log file and check valid input

--- port_scanner.sh
+++ port_scanner.sh
@@ -1,11 +1,32 @@
 #!/bin/bash
-echo "\n"
+echo -e "\n"
 echo "==============================="
-echo "Please enter the ip address to be scanned: "
-read IP_ADDRESS
-echo "Please enter the port number to be scanned: "
-read PORT_NUMBER
+echo "       BASH PORT SCANNER"
+echo "==============================="
+echo -e "\n"
+
+read -p "Please enter the ip address to be scanned: " IP_ADDRESS
+if ! [[ $IP_ADDRESS =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+    echo "Invalid IP address, please try again."
+    exit 1
+fi
+
+read -p "Please enter the port range to be scanned (e.g. 1-80): " PORT_RANGE
+START_PORT=$(echo "$PORT_RANGE" | cut -d'-' -f1)
+END_PORT=$(echo "$PORT_RANGE" | cut -d'-' -f2)
+if ! [[ $START_PORT =~ ^[0-9]+$ ]] || ! [[ $END_PORT =~ ^[0-9]+$ ]] || [ "$START_PORT" -gt "$END_PORT" ]; then
+    echo "Invalid port range, please try again."
+    exit 1
+fi
+
+TIMESTAMP=$(date +"%d%m%y_%H%M%S")
+
 echo "Scanning..."
-nc -vz $IP_ADDRESS $PORT_NUMBER
+
+for ((port=$START_PORT; port<=$END_PORT; port++)); do
+    nc -vz $IP_ADDRESS $port > /dev/null 2>&1 && echo "Port $port - OPEN" || echo "Port $port - CLOSED"
+done > log_${IP_ADDRESS}_${TIMESTAMP}.txt
+
+echo -e "Scanning completed.\nCheck the log_${IP_ADDRESS}_${TIMESTAMP}.txt file for results.\n"
 echo "==============================="
-echo "\n"
+echo -e "\n"
